d2437845499c3e1f7f769b5ed2fb0c459c2fadd2
Shiver <1113673178@qq.com>
2021-05-28 20:52:12 +0800

    Add files via upload

---
+++ Windows/GetAdminAuthorizen.bat
@@ -0,0 +1,6 @@
+%1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit cd /d "%~dp0"
+@echo off
+
+
+echo start success
+pause
---
+++ Windows/MessageSignoutOAOnClose.bat
@@ -0,0 +1,2 @@
+@echo off
+mshta vbscript:msgbox("Remeber SignOut in OA!!!",1,"Message")(window.close)
